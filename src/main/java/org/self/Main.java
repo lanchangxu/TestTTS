379afd7719f328e5eaa2ded6131f10c7ec68d4cf
lcx <793495640@qq.com>
2023-07-31 23:37:18 +0800

    init

--- src/main/java/org/self/Main.java
+++ src/main/java/org/self/Main.java
@@ -1,7 +1,30 @@
 package org.self;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        run();
+    }
+
+
+    private static void run() {
+        System.out.println("1");
+//        WinDef.HWND hwnd = User32.INSTANCE.FindWindow(null, "窗口标题");
+//        if (hwnd != null) {
+//            // 获取窗口大小
+//            WinDef.RECT rect = new WinDef.RECT();
+//            User32.INSTANCE.GetWindowRect(hwnd, rect);
+//            int width = rect.right - rect.left;
+//            int height = rect.bottom - rect.top;
+//            System.out.println("窗口大小: " + width + "x" + height);
+//
+//            // 获取窗口坐标
+//            WinDef.POINT point = new WinDef.POINT();
+//            User32.INSTANCE.GetCursorPos(point);
+//            System.out.println("窗口坐标: (" + point.x + ", " + point.y + ")");
+//        } else {
+//            System.out.println("找不到窗口");
+//        }
     }
 }
